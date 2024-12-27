7bf184afeee7232186944eee81ddb83998d86d82
yeswecan2023 <realvish@gmail.com>
2024-12-27 17:29:53 +0530

    Enhanced for loop

---
+++ foreachExample.java
@@ -0,0 +1,8 @@
+public class foreachExample {
+    public static void main(String[] args) {
+        int arrayOfNumber[] = { 100, 200, 400 };
+        for (int values : arrayOfNumber) {
+            System.out.println(values);
+        }
+    }
+}
