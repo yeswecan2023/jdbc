5b26fea94f576cf78307af40503ac704bbe51d9a
yeswecan2023 <realvish@gmail.com>
2024-12-27 16:15:58 +0530

    Function

---
+++ addtionFunction.java
@@ -0,0 +1,13 @@
+public class addtionFunction {
+
+    public void sum() {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+        addtionFunction add = new addtionFunction();
+        add.sum();
+    }
+}
