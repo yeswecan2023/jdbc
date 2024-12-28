d50eaa1683b19973086fa6066345a74fda4ee33b
yeswecan2023 <realvish@gmail.com>
2024-12-28 15:51:16 +0530

    Constructor

--- ywc.java_tutorials/src/ywc/java_tutorials/Student.java
+++ ywc.java_tutorials/src/ywc/java_tutorials/Student.java
@@ -2,9 +2,20 @@ package ywc.java_tutorials;
 
 public class Student {
 
+	int marks;
+	String name;
+	
+	Student(int a, String b) {
+		marks = a;
+		name = b;
+	}
+	
 	public static void main(String[] args) {
-		int a;
-		System.out.println(a);
+		Student ob1 = new Student(32, "Vish");
+		
+		
+		System.out.println(ob1.marks);
+		System.out.println(ob1.name);
 	}
 
 }
