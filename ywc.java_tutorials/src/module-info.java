86800a5e2143f1a073c923c3f0ce30686fcf6870
yeswecan2023 <realvish@gmail.com>
2024-12-28 12:58:02 +0530

    Eclipse Sample

---
+++ ywc.java_tutorials/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ywc.java_tutorials {
+}
---
+++ ywc.java_tutorials/src/ywc/java_tutorials/Student.java
@@ -0,0 +1,10 @@
+package ywc.java_tutorials;
+
+public class Student {
+
+	public static void main(String[] args) {
+		int a;
+		System.out.println(a);
+	}
+
+}
