ef0449f27b93eafcfad833a4f56a059a0dcaaff3
yeswecan2023 <realvish@gmail.com>
2025-01-05 23:54:06 +0530

    Hash

---
+++ ywc.java_tutorials/src/collection/hashMainClass.java
@@ -0,0 +1,16 @@
+package collection;
+
+import java.util.HashSet;
+
+public class hashMainClass {
+
+	public static void main(String[] args) {
+		HashSet<Integer> hs = new HashSet<Integer>();
+		hs.add(2);
+		hs.add(5);
+		hs.add(2);
+		System.out.println(hs);
+
+	}
+
+}
