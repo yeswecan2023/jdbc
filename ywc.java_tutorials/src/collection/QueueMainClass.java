9dcf36ed2ec64d859e9159b9b9eaae2a8982042f
yeswecan2023 <realvish@gmail.com>
2025-01-05 23:54:15 +0530

    Queue

---
+++ ywc.java_tutorials/src/collection/QueueMainClass.java
@@ -0,0 +1,14 @@
+package collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueMainClass {
+
+	public static void main(String[] args) {
+		Queue<Integer> que = new LinkedList<Integer>();
+		que.add(10);
+		System.out.println(que);
+	}
+
+}
