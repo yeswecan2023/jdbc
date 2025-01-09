46a83628e0dbdbff01f880e9f4cfd0d160bbdd69
yeswecan2023 <realvish@gmail.com>
2025-01-09 22:49:51 +0530

    Sort Based on Age

---
+++ ywc.java_tutorials/src/collection/sortStringMainClass.java
@@ -0,0 +1,59 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Student {
+	
+	int age;
+	String name;
+	
+	
+	
+	public Student(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
+
+
+
+	public String toString() {
+		return "Student [age=" + age + ", name=" + name + "]";
+	}
+	
+	
+}
+
+
+public class sortStringMainClass {
+
+	public static void main(String[] args) {
+		Comparator<Student> com = new Comparator<Student>() {
+			public int compare(Student i, Student j) {
+				if(i.age > j.age) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+		};
+		
+		List<Student> stud = new ArrayList<Student>();
+		stud.add(new Student(21, "Nan"));
+		stud.add(new Student(12, "Jas"));
+		stud.add(new Student(23, "Jok"));
+		stud.add(new Student(78, "Tom"));
+		
+		Collections.sort(stud, com);
+		
+		
+		for ( Student s : stud) {
+			System.out.println(s);
+		}
+		
+	}
+
+}
