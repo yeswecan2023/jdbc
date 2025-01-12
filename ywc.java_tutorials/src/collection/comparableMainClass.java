d61bace529db773d0c98848b0715513c5bc8b991
yeswecan2023 <realvish@gmail.com>
2025-01-12 13:41:47 +0530

    comparable interface

--- ywc.java_tutorials/src/collection/comparableMainClass.java
+++ ywc.java_tutorials/src/collection/comparableMainClass.java
@@ -1,44 +1,41 @@
 package collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-class Student implements Comparable<Student> {
+class Student1 implements Comparable<Student1> {
 	int age;
 	String name;
-	
-	
-	
-	public Student(int age, String name) {
+	public Student1(int age, String name) {
 		this.age = age;
 		this.name = name;
 	}
 
-
 	public String toString() {
-		return "Student [age=" + age + ", name=" + name + "]";
+		return "Student1 [age=" + age + ", name=" + name + "]";
 	}
 
-
-	public int compareTo(Student that) {
-		// TODO Auto-generated method stub
-		return 0;
+	@Override
+	public int compareTo(Student1 that) {
+		if(this.age > that.age) {
+			return 1;
+		} else {
+			return -1;
+		}
 	}
-	
-	
 }
 
 
 public class comparableMainClass {
 
 	public static void main(String[] args) {
-		List<Student> stu = new ArrayList<Student>();
-		stu.add(new Student(20, "asdfasd"));
-		stu.add(new Student(25, "dfawer"));
-		stu.add(new Student(25, "fghjfgyh")); 
-		
-		System.out.println(stu);
-		for(Student s: stu) {
+		List<Student1> stu = new ArrayList<Student1>();
+		stu.add(new Student1(20, "asdfasd"));
+		stu.add(new Student1(30, "dfawer"));
+		stu.add(new Student1(25, "fghjfgyh")); 
+		Collections.sort(stu);
+		for(Student1 s: stu) {
 			System.out.println(s);
 		}
 	}
