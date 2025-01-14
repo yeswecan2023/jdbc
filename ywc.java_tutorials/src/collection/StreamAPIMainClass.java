197165ff41b5aa41f18074e2ccbdfbb84bcbe473
yeswecan2023 <realvish@gmail.com>
2025-01-14 22:52:53 +0530

    Filter

--- ywc.java_tutorials/src/collection/StreamAPIMainClass.java
+++ ywc.java_tutorials/src/collection/StreamAPIMainClass.java
@@ -7,13 +7,13 @@ import java.util.stream.Stream;
 public class StreamAPIMainClass {
 
 	public static void main(String[] args) {
-		List<Integer> nums = Arrays.asList(20,452,6543,212,54);
-		Stream<Integer> s1 =  nums.stream();
+		List<Integer> nums = Arrays.asList(321,54,2,854,320);
+		Stream<Integer> s1 = nums.stream();
 		
+		//s1.forEach(n -> System.out.println(n));
 		
-		s1.forEach(n -> System.out.println(n));
-		
-
+		Stream<Integer> s2 = s1.filter(n -> n%2 == 0);
+		s2.forEach(n -> System.out.println(n));
 	}
 
 }
