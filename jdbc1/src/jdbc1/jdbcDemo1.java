17ec09cc8f0259e8f331908fe33599e0b6a2fa24
yeswecan2023 <realvish@gmail.com>
2025-01-27 23:05:58 +0530

    Insert exception new function introduced

--- jdbc1/src/jdbc1/jdbcDemo1.java
+++ jdbc1/src/jdbc1/jdbcDemo1.java
@@ -8,10 +8,26 @@ import java.sql.Statement;
 public class jdbcDemo1 {
 
 	public static void main(String[] args) throws Exception {
-		readRecord();
+		insertRecord();
 	}
 	
 	public static void readRecord() throws Exception {
+		String url = "jdbc:mysql://localhost:3306/jdbcdemo";
+		String username = "root";
+		String password = "Java@123";
+		String query = "select * from employee ";
+		Connection con = DriverManager.getConnection(url, username, password);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		while (rs.next()) {
+			System.out.println("Id is "+rs.getInt(1));
+			System.out.println("Name is "+rs.getString(2));
+			System.out.println("Salary is "+rs.getInt(3));
+		}
+		con.close();
+	}
+	
+	public static void insertRecord() throws Exception {
 		String url = "jdbc:mysql://localhost:3306/jdbcdemo";
 		String username = "root";
 		String password = "Java@123";
