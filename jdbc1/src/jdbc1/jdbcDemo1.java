d8ed36efc306c93e7f9ab9254f7637f282efd77c
yeswecan2023 <realvish@gmail.com>
2025-01-28 23:00:29 +0530

    Insert to db

--- jdbc1/src/jdbc1/jdbcDemo1.java
+++ jdbc1/src/jdbc1/jdbcDemo1.java
@@ -31,15 +31,12 @@ public class jdbcDemo1 {
 		String url = "jdbc:mysql://localhost:3306/jdbcdemo";
 		String username = "root";
 		String password = "Java@123";
-		String query = "select * from employee ";
+		String query = "Insert into employee values (5, 'das', 25000)";
+		
 		Connection con = DriverManager.getConnection(url, username, password);
 		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(query);
-		while (rs.next()) {
-			System.out.println("Id is "+rs.getInt(1));
-			System.out.println("Name is "+rs.getString(2));
-			System.out.println("Salary is "+rs.getInt(3));
-		}
+		int rows = st.executeUpdate(query);
+		System.out.println(rows);
 		con.close();
 	}
 
