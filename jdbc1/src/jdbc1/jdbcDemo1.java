09470bbebe3afbff2cf8250e567588d22b099c23
yeswecan2023 <realvish@gmail.com>
2025-01-30 22:35:36 +0530

    update

--- jdbc1/src/jdbc1/jdbcDemo1.java
+++ jdbc1/src/jdbc1/jdbcDemo1.java
@@ -1,6 +1,7 @@
 package jdbc1;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,7 +10,7 @@ import java.sql.Statement;
 public class jdbcDemo1 {
 
 	public static void main(String[] args) throws Exception {
-		delete();
+		update();
 	}
 	
 	public static void readRecord() throws Exception {
@@ -64,5 +65,19 @@ public class jdbcDemo1 {
 		System.out.println(rows);
 		con.close();
 	}
+	
+	public static void update() throws Exception {
+		String url = "jdbc:mysql://localhost:3306/jdbcdemo";
+		String username ="root";
+		String password = "Java@123";
+		
+		String query = "update employee set salary = 8000000 where emp_id = 3";
+		
+		Connection con = DriverManager.getConnection(url, username, password);
+		Statement st = con.createStatement();
+		int row = st.executeUpdate(query);
+		System.out.println(row);		
+	} 
+	
 
 }
