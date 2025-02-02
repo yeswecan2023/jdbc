469036d3023bee12c8e5dbabe808363f34374d80
yeswecan2023 <realvish@gmail.com>
2025-02-02 20:11:09 +0530

    leetcode

--- ywc.java_tutorials/src/leetcode/validateParenthesis.java
+++ ywc.java_tutorials/src/leetcode/validateParenthesis.java
@@ -4,40 +4,36 @@ import java.util.Stack;
 
 public class validateParenthesis {
 
-	public static void main(String[] args) {
-		String s = "[[]]";
-		Stack<Character> st = new Stack<Character>();
-		for (int i=0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			System.out.println(" ch "+ch);
-			if(ch == '{' || ch == '(' || ch == '[') {
-				System.out.println(" st "+st);
-				st.push(ch);
-				System.out.println(" st -- "+st);
-			} else {
-				if(st.isEmpty()) {
-					continue;
-				} else if (ch == '}') {
-					if(st.peek() == '{') {
-						st.pop();
-						continue;
-					} else if(st.peek() == '(') {
-						st.pop();
-						continue;
-					} else if(st.peek() == '[') {
-						st.pop();
-						continue;
-					}
-				}
-			}
-		}
-		//System.out.println(st);
-		if(st.size() == 0 ) {
-			System.out.println("Valid");
-		} else {
-			System.out.println("Invalid");
-		}
+    public static void main(String[] args) {
+    	String s = "asdfasdf"; // This should be considered valid
 
-	}
+        Stack<Character> st = new Stack<>();
 
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '{' || ch == '(' || ch == '[') {
+                st.push(ch);
+            } else if (ch == '}' || ch == ')' || ch == ']') { 
+                if (st.isEmpty()) {
+                    System.out.println("Invalid445");
+                    return;
+                }
+
+                char top = st.peek(); // Check the top element
+
+                if ((ch == '}' && top == '{') ||
+                    (ch == ')' && top == '(') ||
+                    (ch == ']' && top == '[')) {
+                    st.pop(); // Pop only if it's a valid match
+                } else {
+                    System.out.println("Invalid");
+                    return;
+                }
+            } 
+            // Else case: Ignore non-bracket characters
+        }
+
+        System.out.println(st.isEmpty() ? "Valid" : "Invalid");
+    }
 }
