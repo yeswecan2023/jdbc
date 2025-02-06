d8f6690e47034b1caac7e60954a04abdb1109cb5
yeswecan2023 <realvish@gmail.com>
2025-02-06 23:01:43 +0530

    Investigation going on

--- ywc.java_tutorials/src/leetcode/ValidateSquare.java
+++ ywc.java_tutorials/src/leetcode/ValidateSquare.java
@@ -1,10 +1,19 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ValidateSquare {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Map<Double, ArrayList<int[]>> map = new HashMap<Double, ArrayList<int[]>>();
+		List<int[]> points = new ArrayList<int[]>();
+		points.add(new int[] {1,2});
+		points.add(new int[] {2,4});
+		points.add(new int[] {3,6});
+		points.add(new int[] {4,8});
 	}
 
 }
