9a8294ccd64c9142ebeade60a65481c47bc3c768
yeswecan2023 <realvish@gmail.com>
2025-02-09 22:48:13 +0530

    loop through array elements

--- ywc.java_tutorials/src/leetcode/removeDuplicateFromSortedArray.java
+++ ywc.java_tutorials/src/leetcode/removeDuplicateFromSortedArray.java
@@ -4,7 +4,10 @@ public class removeDuplicateFromSortedArray {
 
 	public static void main(String[] args) {
 		int[] num = {0,0,1,2,2,3,3,3,4,4};
-
+		int indexForNextNumber = 1;
+		for (int i = 1; i < num.length; i++) {
+			
+		}
 	}
 
 }
