1c1b3f6082105c976432af2c969712b1f6a65980
yeswecan2023 <realvish@gmail.com>
2025-02-10 22:20:41 +0530

    codition added

--- ywc.java_tutorials/src/leetcode/removeDuplicateFromSortedArray.java
+++ ywc.java_tutorials/src/leetcode/removeDuplicateFromSortedArray.java
@@ -6,7 +6,9 @@ public class removeDuplicateFromSortedArray {
 		int[] num = {0,0,1,2,2,3,3,3,4,4};
 		int indexForNextNumber = 1;
 		for (int i = 1; i < num.length; i++) {
-			
+			if (num[i] > num[i-1]) {
+				num[indexForNextNumber] = num[i];
+			}
 		}
 	}
 
