30c56ddfc6a1a98ba04b4dab23a9e727fb95e692
yeswecan2023 <realvish@gmail.com>
2025-02-14 23:38:19 +0530

    Product Of Array Expect Self

---
+++ ywc.java_tutorials/src/leetcode/ProductOfArrayExpectSelf.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ProductOfArrayExpectSelf {
+
+	public static void main(String[] args) {
+		int[] num = { 1,2,3,4};
+		
+
+	}
+
+}
