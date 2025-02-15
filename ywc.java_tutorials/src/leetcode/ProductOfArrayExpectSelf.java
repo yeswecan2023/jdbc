ef8476ad29d71cb9f9419148a9b3455a933bf7ef
yeswecan2023 <realvish@gmail.com>
2025-02-15 23:31:58 +0530

    dev in progress

--- ywc.java_tutorials/src/leetcode/ProductOfArrayExpectSelf.java
+++ ywc.java_tutorials/src/leetcode/ProductOfArrayExpectSelf.java
@@ -7,7 +7,8 @@ public class ProductOfArrayExpectSelf {
 
 	public static void main(String[] args) {
 		int[] num = { 1,2,3,4};
-		
+		int leftProduct[] = new int[num.length];
+		int rightProduct[] = new int[num.length];
 
 	}
 
