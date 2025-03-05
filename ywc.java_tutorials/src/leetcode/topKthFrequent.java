0b04fe56240700e1318db4ef954daf797cd61c75
yeswecan2023 <realvish@gmail.com>
2025-03-05 23:50:31 +0530

    in progress

--- ywc.java_tutorials/src/leetcode/topKthFrequent.java
+++ ywc.java_tutorials/src/leetcode/topKthFrequent.java
@@ -1,9 +1,16 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class topKthFrequent {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] nums = {1,1,2,2}; 
+		Map<Integer, Integer> countMap = new HashMap<>();
+		for (int i=0; i < nums.length; i++) {
+			
+		}
 
 	}
 
